393aa9715b344740d73b45a6b8439ef99fc2ba5a
Clara Yip <clara.yip1@gmail.com>
2019-07-07 23:33:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,22 +2,19 @@ def nyc_pigeon_organizer(data)
   organized_pigeons = {}
 
   data.each do |traits, traits_hash|
-    # search traits_hash for details
+
     traits_hash.each do |details, details_array|
-      # search details_array for names
+
       details_array.each do |name|
 
-         # if organized_pigeons[name] doesn't exist, create empty hash
         if organized_pigeons[name] == nil
           organized_pigeons[name] = {}
         end
 
-         # if organized_pigeons[name][traits] doesn't exist, create empty array
         if organized_pigeons[name][traits] == nil
           organized_pigeons[name][traits] = []
         end
 
-         # shovel details as strings into traits array
         organized_pigeons[name][traits] << details.to_s
 
        end
