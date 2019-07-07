d3f12fae7fad47e59fdfadb1cdc3a22f21491dab
Clara Yip <clara.yip1@gmail.com>
2019-07-07 23:38:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
     value.each do |new_value, names|
       names.each do |name|
         if !new_hash[name]
-          new_hash[name] = {}
+          new_hash[name] = []
         end
         if !new_hash[name][key]
           new_hash[name][key] = []
