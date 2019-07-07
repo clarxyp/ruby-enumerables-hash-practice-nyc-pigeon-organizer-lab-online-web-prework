2155b4cf98ede1cc2858d4474b552a6bbb4da560
Clara Yip <clara.yip1@gmail.com>
2019-07-07 23:32:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+
+organized_pigeons = {}
+
+data.each do |traits, traits_hash|
+  traits_hash.each do |details, details_array|
+    details_array.each do |name|
+      
+      if organized_pigeons[name] == nil 
+        organized_pigeons = {}
+      end
+      
+      if organized_pigeons[name][traits] == nil
+        organized_pigeons[name][traits] = []
+      end
+      
+      organized_pigeons[name][traits] << details.to_s 
+      
+      end 
+    end
+  end 
+
+  organized_pigeons
 end
